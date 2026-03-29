5702f05728d30f268cc66b7cc2fc821d83d2f89c
Michael Shaw <mshaw0@pm.me>
2026-03-29 19:14:15 +0100

    Remove sudo from expand script - call as sudo.

--- scripts/expand.sh
+++ scripts/expand.sh
@@ -1,5 +1,10 @@
-sudo growpart /dev/sda 3
-sudo cryptsetup resize dm_crypt-0
-sudo pvresize /dev/mapper/dm_crypt-0
-sudo lvresize -l +100%FREE /dev/ubuntu-vg/ubuntu-lv
-sudo resize2fs /dev/mapper/ubuntu--vg-ubuntu--lv
+#!/bin/bash
+# This script must be run as root! (e.g. with sudo)
+# This assumes the default partition layout is unchanged, and an encrypted ext4 filesystem is in use.
+
+growpart /dev/sda 3
+cryptsetup resize dm_crypt-0
+pvresize /dev/mapper/dm_crypt-0
+lvresize -l +100%FREE /dev/ubuntu-vg/ubuntu-lv
+resize2fs /dev/mapper/ubuntu--vg-ubuntu--lv
+
---
+++ scripts/personalise.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+#  personalise.sh
+#  
+#
+#  Created by Michael Shaw on 29/03/2026.
+#  
